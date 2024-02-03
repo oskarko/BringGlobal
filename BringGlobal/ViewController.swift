f3e03b7144e756ce8736f1989e45d6358de522b8
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2024-02-03 14:24:09 +0100

    Initial commit

---
+++ BringGlobal/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BringGlobal
+//
+//  Created by Oscar Rodriguez Garrucho on 3/2/24
+//  Linkedin: https://www.linkedin.com/in/oscar-garrucho/
+//  Copyright © 2024 Oscar Rodriguez Garrucho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
